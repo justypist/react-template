e463182f6d8f3ef8c879d4dfda1579a71c9b3b23
谢勇 <xieyong.2021@bytedance.com>
2022-07-14 14:02:35 +0800

    feat(mozart): common store

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,20 @@
 import { FC } from 'react';
 import { useLogger } from './hook/use-logger';
+import { useSum } from './hook/use-sum';
 
 export const App: FC = () => {
   useLogger();
+  const t1 = useSum(10);
+  const t2 = useSum(0);
+  const t3 = useSum(20);
+  const t4 = useSum(30);
 
   return (
     <>
-      <span>Hello World</span>
+      <p>{t1}</p>
+      <p>{t2}</p>
+      <p>{t3}</p>
+      <p>{t4}</p>
     </>
   );
 };
