4eb5e31f613f30afbe092a8767fa824e90e4aecf
谢勇 <xieyong.2021@bytedance.com>
2022-07-27 18:28:15 +0800

    feat: udpate default app page

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,12 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 import { useLogger } from './hook/use-logger';
-
-const latestDateString = () => new Date().toLocaleString();
 
 export const App: FC = () => {
   useLogger({ name: App.name });
 
-  const [text, setText] = useState<string>(latestDateString());
-
   return (
     <>
-      <span>{text}</span>
-      <button onClick={() => setText(latestDateString())}>Refresh</button>
+      <span>Hello World</span>
     </>
   );
 };
