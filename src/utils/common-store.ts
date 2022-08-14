deb89e16db991530f3fed901e58f7c5b11c6719d
simplessor <1003659191@qq.com>
2022-08-14 23:01:21 +0800

    feat: common store添加初始值

--- src/utils/common-store.ts
+++ src/utils/common-store.ts
@@ -1,13 +1,14 @@
-import { BehaviorSubject } from 'rxjs';
 import { debounce } from 'lodash';
+import { BehaviorSubject } from 'rxjs';
 
 type CommonStoreHash<K> = (key: K) => string;
 type CommonStoreObserver<R> = (response: R | undefined) => void;
 
 interface CommonStoreOptions<K, R> {
   hash?: CommonStoreHash<K>;
-  debounceTime?: number;
   fetchData: (keys: K[]) => Promise<R[]>;
+  initialValue?: R;
+  debounceTime?: number;
 }
 
 export class CommonStore<K, R> {
@@ -17,13 +18,20 @@ export class CommonStore<K, R> {
   private _hashSet: Set<string> = new Set<string>();
 
   private _hash: CommonStoreHash<K> = (key: K) => String(key);
-  private _debounceTime = 100;
   private _fetchData: (keys: K[]) => Promise<R[]>;
+  private _initialValue?: R;
+  private _debounceTime = 100;
 
-  constructor({ hash, debounceTime, fetchData }: CommonStoreOptions<K, R>) {
+  constructor({
+    hash,
+    fetchData,
+    initialValue,
+    debounceTime,
+  }: CommonStoreOptions<K, R>) {
     this._hash = hash ?? this._hash;
-    this._debounceTime = debounceTime ?? this._debounceTime;
     this._fetchData = fetchData;
+    this._initialValue = initialValue ?? this._initialValue;
+    this._debounceTime = debounceTime ?? this._debounceTime;
   }
 
   private fetchData = debounce(async () => {
@@ -49,19 +57,32 @@ export class CommonStore<K, R> {
     });
   }, this._debounceTime);
 
+  // 订阅
   public subscribe(key: K, observer: CommonStoreObserver<R>, initialValue?: R) {
+    // 通过请求参数得到hash
     const hash = this._hash(key);
-    // 判断是否已经存在
+    // 通过key查找主题
     let subject = this._hashSubjectMap.get(hash);
+
+    // 如果没找到主题 说明是第一次订阅
     if (!subject) {
+      // 保存hash和key的映射关系
       this._hashKeyMap.set(hash, key);
+      // 将这个hash添加到下一次的请求列表中
       this._hashSet.add(hash);
 
-      subject = new BehaviorSubject<R | undefined>(initialValue);
+      // 生成新的主题， 添加初始值
+      subject = new BehaviorSubject<R | undefined>(
+        initialValue ?? this._initialValue,
+      );
+      // 保存hash和主题的映射关系
       this._hashSubjectMap.set(hash, subject);
     }
 
+    // 发出真实请求
     this.fetchData();
+
+    // 使用传入的观察者订阅这个主题并返回订阅
     return subject.subscribe(observer);
   }
 }
