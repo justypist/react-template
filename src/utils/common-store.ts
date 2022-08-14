3178c32b5d5e3cabde221945f3b81896e80d3528
simplessor <1003659191@qq.com>
2022-08-14 23:19:43 +0800

    refactor: rename

--- src/utils/common-store.ts
+++ src/utils/common-store.ts
@@ -1,18 +1,18 @@
 import { debounce } from 'lodash';
 import { BehaviorSubject } from 'rxjs';
 
-type CommonStoreHash<Request> = (key: Request) => string;
+type CommonStoreHash<Request> = (request: Request) => string;
 type CommonStoreObserver<Response> = (response: Response | undefined) => void;
 
 interface CommonStoreOptions<Request, Response> {
   hash?: CommonStoreHash<Request>;
-  fetchData: (keys: Request[]) => Promise<Response[]>;
+  fetchData: (requests: Request[]) => Promise<Response[]>;
   initialValue?: Response;
   debounceTime?: number;
 }
 
 export class CommonStore<Request, Response> {
-  private _hashKeyMap = new Map<string, Request>();
+  private _hashRequestMap = new Map<string, Request>();
   private _hashSubjectMap = new Map<
     string,
     BehaviorSubject<Response | undefined>
@@ -20,8 +20,9 @@ export class CommonStore<Request, Response> {
 
   private _hashSet: Set<string> = new Set<string>();
 
-  private _hash: CommonStoreHash<Request> = (key: Request) => String(key);
-  private _fetchData: (keys: Request[]) => Promise<Response[]>;
+  private _hash: CommonStoreHash<Request> = (request: Request) =>
+    String(request);
+  private _fetchData: (requests: Request[]) => Promise<Response[]>;
   private _initialValue?: Response;
   private _debounceTime = 100;
 
@@ -37,25 +38,29 @@ export class CommonStore<Request, Response> {
     this._debounceTime = debounceTime ?? this._debounceTime;
   }
 
-  // 真实请求
+  // 合并后的请求
   private fetchData = debounce(async () => {
-    // 获取队列中所有请求key
-    const keys = [...this._hashSet.values()]
-      .map((hash) => this._hashKeyMap.get(hash))
+    // 获取队列中所有request
+    const requests = [...this._hashSet.values()]
+      .map((hash) => this._hashRequestMap.get(hash))
       .filter<Request>(
-        (key): key is Request => key !== null && key !== undefined,
+        (request): request is Request =>
+          request !== null && request !== undefined,
       );
 
+    // 清空请求队列
     this._hashSet.clear();
 
-    const responses = await this._fetchData(keys);
+    // 调用真实请求， 请求内部需要将req和res一一对应返回
+    const responses = await this._fetchData(requests);
 
-    keys.forEach((key, index) => {
+    // 通过相应主题将response派发
+    requests.forEach((request, index) => {
       const response = responses[index];
       if (!response) {
         return;
       }
-      const hash = this._hash(key);
+      const hash = this._hash(request);
       const subject = this._hashSubjectMap.get(hash);
       if (!subject) {
         return;
@@ -66,19 +71,19 @@ export class CommonStore<Request, Response> {
 
   // 订阅
   public subscribe(
-    key: Request,
+    request: Request,
     observer: CommonStoreObserver<Response>,
     initialValue?: Response,
   ) {
-    // 通过请求参数得到hash
-    const hash = this._hash(key);
-    // 通过key查找主题
+    // 通过request得到hash
+    const hash = this._hash(request);
+    // 通过hash查找主题
     let subject = this._hashSubjectMap.get(hash);
 
     // 如果没找到主题 说明是第一次订阅
     if (!subject) {
-      // 保存hash和key的映射关系
-      this._hashKeyMap.set(hash, key);
+      // 保存hash和request的映射关系
+      this._hashRequestMap.set(hash, request);
       // 将这个hash添加到下一次的请求列表中
       this._hashSet.add(hash);
 
