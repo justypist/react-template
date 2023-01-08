1c023090f73ccafb41abf7e5f3c164638f9df4b2
justypist <1003659191@qq.com>
2023-01-08 21:17:30 +0800

    feat: add animation

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,29 @@
 import './app.less';
 
-import { FC } from 'react';
+import { FC, ReactNode, useEffect, useState } from 'react';
 import { useLogger } from './hook/use-logger';
 
 import cloud from './asset/cloud.jpg';
 
+const pages: ReactNode[] = [
+  <img key="cloud" className="cloud" src={cloud} title="cloud" />,
+  <div key="main" className="main">
+    <button onClick={console.log}>click</button>
+  </div>,
+];
+
 export const App: FC = () => {
   useLogger();
 
-  return (
-    <div className="app">
-      <img className="cloud" src={cloud} title="cloud" />
-    </div>
-  );
+  const [pageIndex, setPageIndex] = useState<number>(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setPageIndex(1), 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  const page = pages[pageIndex];
+
+  return <div className="app">{page}</div>;
 };
