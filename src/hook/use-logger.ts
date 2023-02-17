efcf843075ca13930119da19c7b06448424b1bac
打字员 <1003659191@qq.com>
2023-02-17 10:30:47 +0800

    feat: update use logger

--- src/hook/use-logger.ts
+++ src/hook/use-logger.ts
@@ -1,15 +1,8 @@
 export interface UseLoggerOptions {
   name?: string;
-  logOnRender?: boolean;
 }
 
-export const useLogger = (options: UseLoggerOptions = {}) => {
-  const {
-    name = new Error().stack?.split('at')[2].split(' ')[1] ?? 'Unknown',
-    logOnRender = true,
-  } = options;
-
-  if (logOnRender) {
-    console.log(`[${name || useLogger.caller.name}]: rendered`);
-  }
+export const useLogger = (options: UseLoggerOptions) => {
+  const stackName = useMemo(() => new Error().stack?.split('at')[2].split(' ')[1] ?? 'Unknown', []);
+  console.log(`[${options.name ?? stackName}]: rendered`);
 };
