0c389980c9028cffa895ac4559717f63344aa554
打字员 <1003659191@qq.com>
2023-02-17 10:31:37 +0800

    feat: update use logger

--- src/hook/use-logger.ts
+++ src/hook/use-logger.ts
@@ -1,8 +1,13 @@
+import { useMemo } from 'react';
+
 export interface UseLoggerOptions {
   name?: string;
 }
 
 export const useLogger = (options: UseLoggerOptions) => {
-  const stackName = useMemo(() => new Error().stack?.split('at')[2].split(' ')[1] ?? 'Unknown', []);
+  const stackName = useMemo(
+    () => new Error().stack?.split('at')[2].split(' ')[1] ?? 'Unknown',
+    [],
+  );
   console.log(`[${options.name ?? stackName}]: rendered`);
 };
