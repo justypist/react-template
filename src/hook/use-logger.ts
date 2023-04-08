7eef8f915f945287cb7bc22e6f13d6ba4e675131
justypist <1003659191@qq.com>
2023-04-08 19:37:14 +0000

    feat: update use-logger

--- src/hook/use-logger.ts
+++ src/hook/use-logger.ts
@@ -5,5 +5,5 @@ export const useLogger = (name?: string) => {
     () => new Error().stack?.split('at')[6].split(' ')[1] ?? 'Unknown',
     [],
   );
-  console.log(`[${name ?? stackName}]: rendered`);
+  console.debug(`[${name ?? stackName}]: rendered`);
 };
