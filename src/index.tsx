65a991bc19d496e3ded59ca20878b9b5d53174bc
justypist <1003659191@qq.com>
2023-04-08 16:54:25 +0000

    feat: update entrypoint

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,17 @@ import './index.less';
 import { createRoot } from 'react-dom/client';
 import { App } from '@app';
 
-const reactTemplateApp = document.getElementById('react-template-app');
+const entrypoint = document.getElementById('entrypoint');
 
-if (reactTemplateApp) {
-  const root = createRoot(reactTemplateApp);
+(() => {
+  if (!entrypoint) {
+    const tip = document.createElement('strong');
+    tip.textContent = 'entrypoint not found';
+    tip.style.color = 'red';
+    document.body.appendChild(tip);
+    return;
+  }
+
+  const root = createRoot(entrypoint);
   root.render(<App />);
-}
+})();
