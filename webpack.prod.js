3d11eee09926a8f865a32045f23a04d68b20cbec
justypist <1003659191@qq.com>
2023-04-08 18:31:10 +0000

    chore: add css-minimizer

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,15 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
   optimization: {
     runtimeChunk: 'single',
+    minimizer: [
+      '...',
+      new CssMinimizerPlugin(),
+    ],
   },
 });
