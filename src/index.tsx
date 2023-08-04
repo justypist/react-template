8777fb89ff724ffbaa94d9990d26dd5dda56fbf8
typist <1003659191@qq.com>
2023-08-04 01:12:32 +0000

    feat: split service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,7 @@ import { StrictMode } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import { router } from '@router';
 
-if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((registration) => {
-        // console.log('SW registered: ', registration);
-      })
-      .catch((registrationError) => {
-        // console.log('SW registration failed: ', registrationError);
-      });
-  });
-}
+import './service-worker';
 
 const entrypoint = document.getElementById('entrypoint');
 
